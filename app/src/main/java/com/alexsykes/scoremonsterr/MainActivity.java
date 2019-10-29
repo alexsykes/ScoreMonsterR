d83557a98d641598c705e1abd1bb52252222b9ab
Alex Sykes <alex@alexsykes.net>
2019-10-29 08:24:52 +0000

    Initial commit

---
+++ app/src/main/java/com/alexsykes/scoremonsterr/MainActivity.java
@@ -0,0 +1,14 @@
+package com.alexsykes.scoremonsterr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
