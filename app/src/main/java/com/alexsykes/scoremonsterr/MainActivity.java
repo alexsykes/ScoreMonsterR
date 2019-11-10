73a02da66c502db0e6f4cfdc95575f0a03aec5d9
Alex Sykes <alex@alexsykes.net>
2019-11-10 21:49:43 +0000

    MainActivity extends FragmentActivity

--- app/src/main/java/com/alexsykes/scoremonsterr/MainActivity.java
+++ app/src/main/java/com/alexsykes/scoremonsterr/MainActivity.java
@@ -1,17 +1,18 @@
 package com.alexsykes.scoremonsterr;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.ListPreference;
-import androidx.preference.PreferenceManager;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.preference.PreferenceManager;
+
+public class MainActivity extends FragmentActivity {
     public static final int TEXT_REQUEST = 1;
     SharedPreferences prefs;
     TextView statusLine;
@@ -32,15 +33,16 @@ public class MainActivity extends AppCompatActivity {
         boolean observer = sharedPreferences.getBoolean("observer", false);
 
         // If trialId not set, then go to SettingsActivity
-        if(trialId.equals("")  ){
+        if (trialId.equals("")) {
             goSetup();
         }
 
         // If observer, then check for observer name and section
-        else if(observer && ((section.equals("")) || (name.equals(""))) ){
+        else if (observer && ((section.equals("")) || (name.equals("")))) {
             goSetup();
         }
 
+        TouchFragment touchFragment = new TouchFragment();
         statusLine.setText("Trial id: " + trialId);
     }
 
@@ -49,9 +51,16 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, TEXT_REQUEST);
     }
 
-    public void enterScore(View view) {
+    public void countDabs(View view) {
     }
 
-    public void addDigit(View view) {
+    public static class TouchFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_score_pad, container, false);
+        }
     }
+
 }
