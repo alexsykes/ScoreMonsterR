a13810365ac80291986ca66023372b9d3e29ee94
Alex Sykes <alex@alexsykes.net>
2019-11-10 10:21:25 +0000

    Fragment resource XML files added

--- app/src/main/java/com/alexsykes/scoremonsterr/MainActivity.java
+++ app/src/main/java/com/alexsykes/scoremonsterr/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -40,13 +41,17 @@ public class MainActivity extends AppCompatActivity {
             goSetup();
         }
 
-
         statusLine.setText("Trial id: " + trialId);
-
     }
 
     private void goSetup() {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    public void enterScore(View view) {
+    }
+
+    public void addDigit(View view) {
     }
 }
