d8ed6161499162b6595aaedb13b8283d8470fe42
Alex <alex@alexsykes.net>
2019-11-14 17:03:02 +0000

    Update

--- app/src/main/java/com/alexsykes/scoremonsterr/MainActivity.java
+++ app/src/main/java/com/alexsykes/scoremonsterr/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentTransaction;
@@ -13,7 +15,7 @@ import androidx.preference.PreferenceManager;
 public class MainActivity extends FragmentActivity {
     public static final int TEXT_REQUEST = 1;
     SharedPreferences prefs;
-    TextView statusLine;
+    TextView statusLine, scoreView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +30,8 @@ public class MainActivity extends FragmentActivity {
         String name = sharedPreferences.getString("name", "");
         String section = sharedPreferences.getString("section", "");
 
-        boolean observer = sharedPreferences.getBoolean("observer", false);
+        boolean iSobserver = sharedPreferences.getBoolean("isObserver", false);
+        boolean useScorePad = sharedPreferences.getBoolean("useScorePad", false);
 
         // If trialId not set, then go to SettingsActivity
         if (trialId.equals("")) {
@@ -36,13 +39,19 @@ public class MainActivity extends FragmentActivity {
         }
 
         // If observer, then check for observer name and section
-        else if (observer && ((section.equals("")) || (name.equals("")))) {
+        else if (iSobserver && ((section.equals("")) || (name.equals("")))) {
             goSetup();
         }
 
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
         ft.replace(R.id.top, new NumberPadFragment());
-        ft.replace(R.id.bottom, new TouchFragment());
+
+        if (useScorePad) {
+            ft.replace(R.id.bottom, new ScorePadFragment());
+        } else {
+            ft.replace(R.id.bottom, new TouchFragment());
+        }
         ft.commit();
 
 
@@ -56,5 +65,34 @@ public class MainActivity extends FragmentActivity {
     }
 
     public void addDigit(View view) {
+        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    public int countDabs(View view) {
+        scoreView = findViewById(R.id.scoreView);
+        int score = 0;
+        int intID = view.getId();
+        Button button = view.findViewById(intID);
+        String digit = button.getText().toString();
+
+        switch (digit) {
+            case "Clean":
+                score = 0;
+                break;
+            case "Ten":
+                score = 10;
+                break;
+            case "Five":
+                score = 5;
+                break;
+            case "Dab":
+                if (score < 3)
+                    score++;
+                break;
+        }
+        scoreView.setText(String.valueOf(score));
+        return score;
     }
 }
